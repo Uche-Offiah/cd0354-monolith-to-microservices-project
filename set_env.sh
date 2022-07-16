626ae222d1683c1217715963b989d0f51eb736d9
Uche Offiah <uoffiah@bluetag-it.com>
2022-07-16 17:46:32 +0100

    Broke udagram monolith project into microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=UchechukwuOffiah
+export POSTGRES_PASSWORD=UchechukwuOffiah35
+export POSTGRES_HOST=uchechukwuoffiah-dev.c6ypozmldpt5.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=uche-1975146-dev
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=UchechukwuOffiah
 export JWT_SECRET=testing
 export URL=http://localhost:8100
